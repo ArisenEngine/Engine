f336e4e8209432a0ab8953893ce2b182a4d31032
eatdreamcat <meow.std@outlook.com>
2023-11-06 00:00:46 +0800

    Project Solution

---
+++ Nebula/Editor/EditorLib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EditorLib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Nebula/Engine/NebulaEngine/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NebulaEngine
+{
+    public static class Application
+    {
+        public static string dataPath = string.Empty;
+        public static bool isRunning = false;
+    }
+}
