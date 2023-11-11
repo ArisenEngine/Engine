00bddc8ec56c7072d7f43307dfc55637304d6386
eatdreamcat <meow.std@outlook.com>
2023-11-11 19:42:12 +0800

    TreeViewGrid

---
+++ Nebula/Editor/NebulaEditor/Views/Utilities/HierarchyView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NebulaEditor.Views.Utilities;
+
+public partial class HierarchyView : UserControl
+{
+    public HierarchyView()
+    {
+        InitializeComponent();
+    }
+}
