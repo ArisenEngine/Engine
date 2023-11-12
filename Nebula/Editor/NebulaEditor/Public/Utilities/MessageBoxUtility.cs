7d907d03caf6a8f95fa36e8530f1907206af757b
eatdreamcat <meow.std@outlook.com>
2023-11-12 23:47:20 +0800

    Hierarchy and Fluent theme , and Icon

---
+++ Nebula/Editor/NebulaEditor/Public/Utilities/MessageBoxUtility.cs
@@ -0,0 +1,25 @@
+﻿using MsBox.Avalonia.Enums;
+using MsBox.Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace NebulaEditor.Utilities
+{
+    public static class MessageBoxUtility
+    {
+        public static async Task ShowMessageBoxStandard(string title, string text, ButtonEnum @enum = ButtonEnum.Ok, 
+            Icon icon = Icon.None, WindowStartupLocation windowStartupLocation = WindowStartupLocation.CenterScreen)
+        {
+            var box = MessageBoxManager
+            .GetMessageBoxStandard(title, text,
+                @enum);
+
+            var task = box.ShowAsync();
+            await task;
+        }
+    }
+}
