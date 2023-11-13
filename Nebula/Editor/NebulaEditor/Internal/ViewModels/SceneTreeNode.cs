06d79de81b5b43f8e332c2c62977269e0cb2d19a
jingwen.wang <jingwen.wang@dena.com>
2023-11-13 14:19:21 +0800

    Hierarchy

--- Nebula/Editor/NebulaEditor/Internal/ViewModels/SceneTreeNode.cs
+++ Nebula/Editor/NebulaEditor/Internal/ViewModels/SceneTreeNode.cs
@@ -1,11 +1,61 @@
-﻿using NebulaEditor.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using Avalonia.Media.Imaging;
+using NebulaEditor.Models;
 
 namespace NebulaEditor.ViewModels;
 
 public class SceneTreeNode : TreeNodeBase
 {
-    public SceneTreeNode(string name, string path, bool isBranch, bool isRoot = false) : base(name, path, isBranch, isRoot)
+    public SceneTreeNode(string name, string path, bool isBranch, bool isRoot = false) : base(name, path, isBranch, isRoot, false)
     {
         
     }
+
+    private ObservableCollection<SceneTreeNode> m_Children;
+    private ObservableCollection<SceneTreeNode> LoadChildren()
+    {
+        if (!IsBranch)
+        {
+            throw new NotSupportedException();
+        }
+        
+        var options = new EnumerationOptions
+        {
+            IgnoreInaccessible = true ,
+            AttributesToSkip = FileAttributes.Hidden | FileAttributes.System
+        };
+        var result = new ObservableCollection<SceneTreeNode>();
+
+        foreach (var d in Directory.EnumerateDirectories(Path, "*", options))
+        {
+            var name = d.Split(System.IO.Path.DirectorySeparatorChar)[^1];
+            result.Add(new SceneTreeNode(name, d, true, false));
+        }
+        
+        if (result.Count == 0)
+        {
+            HasChildren = false;
+        }
+
+        return result;
+    }
+    
+    public override IReadOnlyList<SceneTreeNode> Children<SceneTreeNode>()
+    {
+        if (m_Children == null)
+        {
+            m_Children = LoadChildren();
+        }
+
+        return (IReadOnlyList<SceneTreeNode>) m_Children;
+    }
+
+    protected override string LeafIconPath => "avares://NebulaEditor/Assets/Icons/entity-icon.png";
+    protected override string BranchIconPath => "avares://NebulaEditor/Assets/Icons/entity-icon.png";
+    protected override string BranchOpenIconPath => "avares://NebulaEditor/Assets/Icons/entity-icon.png";
+    protected override string RootIconPath => "avares://NebulaEditor/Assets/Icons/clapperboard.png";
+    
 }
--- Nebula/Editor/NebulaEditor/Internal/ViewModels/WorldHierarchyViewModel.cs
+++ Nebula/Editor/NebulaEditor/Internal/ViewModels/WorldHierarchyViewModel.cs
@@ -7,11 +7,40 @@ using NebulaEngine;
 
 namespace NebulaEditor.ViewModels;
 
-public class WorldHierarchyViewModel : HierarchyViewModel
+public class WorldHierarchyViewModel : ViewModelBase, IHierarchyVM<SceneTreeNode>
 {
-    protected override string Header => null;
+    public string Header => "Hierarchy";
 
-    protected override void InitializeRoots()
+    private HierarchicalTreeDataGridSource<SceneTreeNode> m_Source;
+
+    public HierarchicalTreeDataGridSource<SceneTreeNode> Source
+    {
+        get
+        {
+            if (m_Source == null)
+            {
+                InitializeSource();
+            }
+
+            return m_Source;
+        }
+    }
+
+    private SceneTreeNode[] m_Roots;
+    public SceneTreeNode[] Roots
+    {
+        get
+        {
+            if (m_Roots == null)
+            {
+                InitializeRoots();
+            }
+
+            return m_Roots;
+        }
+    }
+    
+    private void InitializeRoots()
     {
         m_Roots = new SceneTreeNode[2]
         {
@@ -20,26 +49,26 @@ public class WorldHierarchyViewModel : HierarchyViewModel
         };
     }
 
-    protected override void InitializeSource()
+    private void InitializeSource()
     {
-        m_Source = new HierarchicalTreeDataGridSource<TreeNodeBase>(Array.Empty<SceneTreeNode>())
+        m_Source = new HierarchicalTreeDataGridSource<SceneTreeNode>(Array.Empty<SceneTreeNode>())
         {
             Columns =
             {
-                new HierarchicalExpanderColumn<TreeNodeBase>(
-                    new TemplateColumn<TreeNodeBase>(
+                new HierarchicalExpanderColumn<SceneTreeNode>(
+                    new TemplateColumn<SceneTreeNode>(
                         Header,
                         "NameCell",
                         "NameEditCell",
                         new GridLength(1, GridUnitType.Star),
-                        new TemplateColumnOptions<TreeNodeBase>()
+                        new TemplateColumnOptions<SceneTreeNode>()
                         {
                             CompareAscending = null,
                             CompareDescending = null,
                             IsTextSearchEnabled = true,
                             TextSearchValueSelector = x => x.Name
                         }),
-                    x => x.Children,
+                    x => x.Children<SceneTreeNode>(),
                     x => x.HasChildren,
                     x => x.IsExpanded
                 ),
@@ -51,4 +80,6 @@ public class WorldHierarchyViewModel : HierarchyViewModel
         Source.RowSelection!.SingleSelect = false;
         Source.Items = Roots;
     }
+
+    
 }
