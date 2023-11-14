040e2c202d3c9a65b82e61a76f8da74d0c48ed72
jingwen.wang <jingwen.wang@dena.com>
2023-11-14 21:07:53 +0800

    Explorer open

--- Nebula/Editor/NebulaEditor/Internal/MenuItemEntries/ProjectContextMenuEntries.cs
+++ Nebula/Editor/NebulaEditor/Internal/MenuItemEntries/ProjectContextMenuEntries.cs
@@ -1,6 +1,19 @@
+using System.Diagnostics;
+using Avalonia.Interactivity;
+using NebulaEditor.Attributes;
+using NebulaEditor.ViewModels;
+
 namespace NebulaEditor.Internal.MenuItemEntries;
 
 internal static class ProjectContextMenuEntries
 {
-    
+    [MenuItem("Project/Show in Explorer")]
+    public static void ShowInExplorer(object? sender, RoutedEventArgs e)
+    {
+        if (sender != null)
+        {
+            FolderTreeNode treeNode = ((Avalonia.Controls.MenuItem) sender).DataContext as FolderTreeNode;
+            Process.Start("explorer.exe", $"/select,\"{treeNode.Path}\"");
+        }
+    }
 }
