68317ce93a8adbe46d45386202c118388be37990
jingwen.wang <jingwen.wang@dena.com>
2023-11-16 21:01:29 +0800

    DataGrid Log Console

---
+++ Nebula/Editor/NebulaEditor/Internal/Views/UserControl1.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NebulaEditor.Internal.Views;
+
+public partial class UserControl1 : UserControl
+{
+    public UserControl1()
+    {
+        InitializeComponent();
+    }
+}
