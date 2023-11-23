3da5aa6958e7ff3cb75ad7b63ea193ab69164214
eatdreamcat <meow.std@outlook.com>
2023-11-23 17:19:32 +0800

    add CS trace to log

--- Nebula/Engine/NebulaEngine/API/DebuggerAPI.cs
+++ Nebula/Engine/NebulaEngine/API/DebuggerAPI.cs
@@ -8,22 +8,22 @@ namespace NebulaEngine.API
         private const string ENGINE_DLL = @"Engine.dll";
 
         [DllImport(ENGINE_DLL, EntryPoint = @"Debugger_Log", CharSet = CharSet.Unicode)]
-        public static extern void Debugger_Log(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName);
+        internal static extern void Debugger_Log(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName, [MarshalAs(UnmanagedType.LPStr)] string csharpStackTrace);
 
         [DllImport(ENGINE_DLL, EntryPoint = @"Debugger_Info", CharSet = CharSet.Unicode)]
-        public static extern void Debugger_Info(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName);
+        internal static extern void Debugger_Info(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName, [MarshalAs(UnmanagedType.LPStr)] string csharpStackTrace);
 
         [DllImport(ENGINE_DLL, EntryPoint = @"Debugger_Trace", CharSet = CharSet.Unicode)]
-        public static extern void Debugger_Trace(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName);
+        internal static extern void Debugger_Trace(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName, [MarshalAs(UnmanagedType.LPStr)] string csharpStackTrace);
 
         [DllImport(ENGINE_DLL, EntryPoint = @"Debugger_Warning", CharSet = CharSet.Unicode)]
-        public static extern void Debugger_Warning(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName);
+        internal static extern void Debugger_Warning(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName, [MarshalAs(UnmanagedType.LPStr)] string csharpStackTrace);
 
         [DllImport(ENGINE_DLL, EntryPoint = @"Debugger_Error", CharSet = CharSet.Unicode)]
-        public static extern void Debugger_Error(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName);
+        internal static extern void Debugger_Error(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName, [MarshalAs(UnmanagedType.LPStr)] string csharpStackTrace);
 
         [DllImport(ENGINE_DLL, EntryPoint = @"Debugger_Fatal", CharSet = CharSet.Unicode)]
-        public static extern void Debugger_Fatal(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName);
+        internal static extern void Debugger_Fatal(string msg, [MarshalAs(UnmanagedType.LPStr)] string threadName, [MarshalAs(UnmanagedType.LPStr)] string csharpStackTrace);
     }
 
 }
--- Nebula/Engine/NebulaEngine/API/RenderSurfaceAPI.cs
+++ Nebula/Engine/NebulaEngine/API/RenderSurfaceAPI.cs
@@ -12,20 +12,20 @@ namespace NebulaEngine.API
         private const string ENGINE_DLL = "Engine.dll";
 
         [DllImport(ENGINE_DLL, EntryPoint = "CreateRenderSurface")]
-        public static extern int CreateRenderSurface(IntPtr host, IntPtr messageHandle, int width, int height);
+        internal static extern int CreateRenderSurface(IntPtr host, IntPtr messageHandle, int width, int height);
         
         // TODO: implement in cpp file
         [DllImport(ENGINE_DLL, EntryPoint = "CreateFullScreenRenderSurface")]
-        public static extern int CreateFullScreenRenderSurface(IntPtr host, IntPtr messageHandle);
+        internal static extern int CreateFullScreenRenderSurface(IntPtr host, IntPtr messageHandle);
 
         [DllImport(ENGINE_DLL, EntryPoint = "RemoveRenderSurface")]
-        public static extern void RemoveRenderSurface(int surfaceId);
+        internal static extern void RemoveRenderSurface(int surfaceId);
 
         [DllImport(ENGINE_DLL, EntryPoint = "ResizeRenderSurface")]
-        public static extern void ResizeRenderSurface(int surfaceId);
+        internal static extern void ResizeRenderSurface(int surfaceId);
 
         [DllImport(ENGINE_DLL, EntryPoint = "GetWindowHandle")]
-        public static extern IntPtr GetWindowHandle(int surfaceId);
+        internal static extern IntPtr GetWindowHandle(int surfaceId);
 
 
     }
