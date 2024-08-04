c134628e057fca1b119e9ec2ee32f2ebefa8afcb
eatdreamcat <meow.std@outlook.com>
2024-08-05 00:02:17 +0800

    test

--- Nebula/Test/EngineTest/Shader/FullScreen.hlsl
+++ Nebula/Test/EngineTest/Shader/FullScreen.hlsl
@@ -14,7 +14,7 @@ Varying Vert(Attribute input, uint vertexId : SV_VertexID)
 {
     Varying output = (Varying)0;
     // 00, 01, 10
-    output.positionCS = float4((vertexId >> 1) << 1, (vertexId << 1) & 2, 0, 1.0);
+    output.positionCS = float4((vertexId >> 1) << 1, (vertexId << 1) & 2, 0.5, 1.0);
 
     // 计算 UV 坐标
     output.uv = float2(vertexId / 3.0, vertexId / 3.0);
