3739db4bd59f411f39acd00e4790c8399c94fe3a
eatdreamcat <meow.std@outlook.com>
2024-09-26 17:21:09 +0800

    remove cppsharp test project

--- Nebula/Test/CppSharpInvokerTest/Debugger-symbols.cpp
+++
@@ -1,9 +0,0 @@
-#include <Common/CommandHeaders.h>
-#include <DllHeader.h>
-#include <Logger\Logger.h>
-#include <new>
-
-bool (*_0)(unsigned int) = &NebulaEngine::ID::IsValid;
-unsigned int (*_1)(unsigned int) = &NebulaEngine::ID::Index;
-unsigned int (*_2)(unsigned int) = &NebulaEngine::ID::Generation;
-unsigned int (*_3)(unsigned int) = &NebulaEngine::ID::NewGeneration;
