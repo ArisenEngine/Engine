2dd533fc08671ab6422d3fc8bd28073db9a6b343
eatdreamcat <meow.std@outlook.com>
2025-05-26 14:11:09 +0800

    生成sln

--- Arisen/Test/CppSharpGeneratorTest/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using CppSharp;
-using CppSharpCsharp;
-
-Console.WriteLine("##### CppSharp Start #####");
-ConsoleDriver.Run(new EngineLibrary());
