fe6a73150955041ada2d91858c4c016d57d1903d
eatdreamcat <meow.std@outlook.com>
2025-06-08 22:21:14 +0800

    upload dx smaple for test

---
+++ Arisen/Test/DXTest/src/DXTester.cpp
@@ -0,0 +1,46 @@
+﻿#include "DXTester.h"
+
+#include "Logger/Logger.h"
+
+using namespace ArisenEngine::Debugger;
+
+void MyMethod() {
+#if defined(__clang__) || defined(__GNUC__)
+    std::cout << "Function: " << __PRETTY_FUNCTION__ << std::endl;
+#elif defined(_MSC_VER)
+    std::cout << "Function: " << __FUNCSIG__ << std::endl;
+#else
+    std::cout << "Function: " << __func__ << std::endl;
+#endif
+}
+
+void DXTester::Initialize()
+{
+    if (!Logger::GetInstance().Initialize())
+    {
+        throw std::exception(" Logger initialize failed.");
+    }
+
+    MyMethod();
+    LOG_INFO("DXTester::Initialize done");
+}
+
+void DXTester::EngineLoop()
+{
+    // LOG_INFO("DXTester::EngineLoop");
+    
+}
+
+void DXTester::OnKeyDown(UINT8 KeyCode)
+{
+}
+
+void DXTester::OnKeyUp(UINT8 KeyCode)
+{
+}
+
+void DXTester::ShutDown()
+{
+    LOG_INFO("DXTester::Shutdown");
+    Logger::Shutdown();
+}
