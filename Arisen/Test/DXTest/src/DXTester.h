081715f2eb09d3b963cc2e08a95c9fcc9cfb92b2
UWhisper <861314114@qq.com>
2025-06-08 22:21:14 +0800

    upload dx smaple for test

---
+++ Arisen/Test/DXTest/src/DXTester.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "windows.h"
+
+class DXTester
+{
+public:
+    void Initialize();
+    void EngineLoop();
+
+    void OnKeyDown(UINT8 KeyCode);
+    void OnKeyUp(UINT8 KeyCode);
+    void ShutDown();
+};
