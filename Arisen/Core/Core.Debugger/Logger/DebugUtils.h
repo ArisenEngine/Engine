26f1565b9c7e45d950bab0862e0a490784dde0d5
Whisper <861314114@qq.com>
2025-07-21 18:48:55 +0800

    Update DebugUtils.h

--- Arisen/Core/Core.Debugger/Logger/DebugUtils.h
+++ Arisen/Core/Core.Debugger/Logger/DebugUtils.h
@@ -1,15 +1,28 @@
 ﻿#pragma once
+#include "Logger.h"
 
 #ifdef ARISEN_DEBUG
-12
+
+
+
+
+
 #else
-12
+
+// debug log func and line         
+#define ASSERTION_FAILED(Message, ...)    \
+    do                                  \
+    {                                   \
+        LOG_FATAL(Message);             \
+    } while (0)
+
+#define CHECK(Expr, Message, ...)    \
+    do                          \
+    {                           \
+        if (!(Expr))            \
+        {                       \
+            ASSERTION_FAILED(Message,##__VA_ARGS__); \
+        }                       \
+    } while (false)             
 #endif
 
-
-class DebugUtils
-{
-public:
-
-    
-};
