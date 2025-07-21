48f3210688c95313528656ed65fbb5f9b20db716
eatdreamcat <meow.std@outlook.com>
2025-07-21 18:48:24 +0800

    Update RHIFactoryD3D12.cpp

--- Arisen/Core/ArisenRHID3D12/src/RHIFactoryD3D12.cpp
+++ Arisen/Core/ArisenRHID3D12/src/RHIFactoryD3D12.cpp
@@ -1,5 +1,11 @@
 ﻿#include "RHIFactoryD3D12.h"
+
+#include <wrl/client.h>
+
 #include "RHIFactoryBase.h"
+#include "Logger/DebugUtils.h"
+
+using Microsoft::WRL::ComPtr;
 
 ARISENRHI_BEGIN_NAMEPSACE
 class RHIFactoryD3D12Impl : public RHIFactoryBase<IRHIFactoryD3D12>
@@ -12,24 +18,38 @@ public:
     }
     
     RHIFactoryD3D12Impl()
-        :RHIFactoryD3D12Impl()
+        :RHIFactoryBase()
     {
         
     }
 
-    virtual void CreateDeviceAndContextD3D12(IDevice** pOutDevice, IDeviceContext** pOutDeviceContext) override;
-    virtual void CreateSwapChainD3D12(IDevice* pDevice, IDeviceContext* pContext, ISwapChain** pOutSwapChain) override;
+    virtual void CreateDeviceD3D12(const EngineCreateInfoD3D12& InCreateInfo, IDevice** pOutDevice) override;
+    virtual void CreateSwapChainD3D12(IDevice* pDevice, ISwapChain** pOutSwapChain) override;
 };
 
-void RHIFactoryD3D12Impl::CreateSwapChainD3D12(IDevice* pDevice, IDeviceContext* pContext, ISwapChain** pOutSwapChain)
+void RHIFactoryD3D12Impl::CreateSwapChainD3D12(IDevice* pDevice, ISwapChain** pOutSwapChain)
 {
 }
 
-void RHIFactoryD3D12Impl::CreateDeviceAndContextD3D12(IDevice** pOutDevice, IDeviceContext** pOutDeviceContext)
+void RHIFactoryD3D12Impl::CreateDeviceD3D12(const EngineCreateInfoD3D12& InCreateInfo, IDevice** pOutDevice)
 {
-    //TODO: Check version.
+    //TODO: Check Engine version.
 
-    
+   CHECK(pOutDevice, "pOutDevice is nullptr.");
+
+    try
+    {
+        // enable debug layer.
+        if (InCreateInfo.EnableValidation)
+        {
+            ComPtr<ID3D12Debug> debugController;
+        }
+    }
+    catch (const std::runtime_error& e)
+    {
+        LOG_ERROR("failed to create DeviceD3D12.");
+        return;
+    }
 }
 
 IRHIFactoryD3D12* CreateRHIFactoryD3D12()
