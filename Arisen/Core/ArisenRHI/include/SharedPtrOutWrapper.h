76bbb94c5ae00a2d2a0c4d86cbc6b90d314b1f65
eatdreamcat <meow.std@outlook.com>
2025-07-21 18:44:12 +0800

    Update SharedPtrOutWrapper.h

--- Arisen/Core/ArisenRHI/include/SharedPtrOutWrapper.h
+++ Arisen/Core/ArisenRHI/include/SharedPtrOutWrapper.h
@@ -1,18 +1,33 @@
-﻿#pragma once
+#pragma once
 #include <memory>
 
+#include "RHIMacros.h"
+
+ARISENRHI_BEGIN_NAMEPSACE
 template<typename T>
 class SharedPtrOutWrapper
 {
 public:
-    SharedPtrOutWrapper(std::shared_ptr<T>& ptr);
-
-    ~SharedPtrOutWrapper();
-
-    T** operator&();
+    SharedPtrOutWrapper(std::shared_ptr<T>& ptr)
+    :ptrRef(ptr), rawPtr(nullptr)
+    {
+        ptrRef.reset();
+    }
+    
+    ~SharedPtrOutWrapper()
+    {
+        if (rawPtr)
+        {
+            ptrRef.reset(rawPtr);
+        }
+    }
+    
+    T** operator&()
+    {
+        return &rawPtr;
+    }
 private:
     std::shared_ptr<T>& ptrRef;
     T* rawPtr;
 };
-
-
+ARISENRHI_END_NAMESPACE
