64ec67455d8aec9281425abe0fa46e2e1659266c
Whisper <861314114@qq.com>
2025-07-24 19:11:34 +0800

    Update RHIMacrosD3D12.h

--- Arisen/Core/ArisenRHID3D12/include/RHIMacrosD3D12.h
+++ Arisen/Core/ArisenRHID3D12/include/RHIMacrosD3D12.h
@@ -3,3 +3,11 @@
 
 using ArisenEngine::Debugger::Logger;
 
+#define CHECK_D3D_HR(hr, Message)\
+    do\
+    {\
+        if (FAILED(hr))\
+        {\
+            LOG_RHI_FATAL_AND_THROW(Message);\
+        }\
+    } while (0)
