0dea7cebc522338c9395b614c142a7a216b93cb1
eatdreamcat <meow.std@outlook.com>
2025-07-28 18:51:16 +0800

    Add files via upload

---
+++ Arisen/Core/ArisenRHI/include/DeviceBase.cpp
@@ -0,0 +1,7 @@
+#include "DeviceBase.h"
+
+ArisenRHI::DeviceBase::DeviceBase(const std::string& adapter_name)
+    : m_adapter_name(adapter_name)
+{
+    
+}
---
+++ Arisen/Core/ArisenRHI/include/DeviceBase.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "IDevice.h"
+#include "ObjectBase.h"
+#include "RHIMacros.h"
+
+ARISENRHI_BEGIN_NAMEPSACE
+class DeviceBase : public ObjectBase, public IDevice
+{
+public:
+    DeviceBase(const std::string& adapter_name);
+private:
+    const std::string m_adapter_name;
+    
+};
+ARISENRHI_END_NAMESPACE
