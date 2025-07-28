b1d8ecc3c3169bae724d4a42c635b02fc16c51ec
eatdreamcat <meow.std@outlook.com>
2025-07-28 19:22:38 +0800

    Update DeviceD3D12.cpp

--- Arisen/Core/ArisenRHID3D12/src/DeviceD3D12.cpp
+++ Arisen/Core/ArisenRHID3D12/src/DeviceD3D12.cpp
@@ -1,8 +1,21 @@
 #include "DeviceD3D12.h"
 
-ArisenRHI::DeviceD3D12::DeviceD3D12(const std::string& adapter_name, const ComPtr<ID3D12Device>& device_cptr)
-    :DeviceBase(adapter_name)
+#include <codecvt>
+
+ARISENRHI_BEGIN_NAMEPSACE
+    static std::string GetAdapterNameDxgi(IDXGIAdapter& adapter)
+{
+    DXGI_ADAPTER_DESC desc{};
+    adapter.GetDesc(&desc);
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.to_bytes(desc.Description);
+}
+
+DeviceD3D12::DeviceD3D12(const ComPtr<IDXGIAdapter>& adapter_cptr, const ComPtr<ID3D12Device>& device_cptr)
+    :DeviceBase(GetAdapterNameDxgi(*adapter_cptr.Get()))
     ,m_device_cptr(device_cptr)
 {
     
 }
+
+ARISENRHI_END_NAMESPACE
