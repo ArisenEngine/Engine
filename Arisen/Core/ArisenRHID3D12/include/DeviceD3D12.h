6fa0300aa2ccae099ebb6145c828dd78b46221c0
eatdreamcat <meow.std@outlook.com>
2025-07-28 19:22:19 +0800

    Update DeviceD3D12.h

--- Arisen/Core/ArisenRHID3D12/include/DeviceD3D12.h
+++ Arisen/Core/ArisenRHID3D12/include/DeviceD3D12.h
@@ -1,20 +1,16 @@
 #pragma once
 #include <d3d12.h>
+#include <dxgi.h>
 #include <wrl/client.h>
 
 #include "DeviceBase.h"
-#include "RHIMacros.h"
-#include "RHIMacrosD3D12.h"
-
-ARISENRHI_BEGIN_NAMEPSACE
-
 using namespace Microsoft::WRL;
-
 class DeviceD3D12 : public DeviceBase
 {
 public:
-    DeviceD3D12(const std::string& adapter_name, const ComPtr<ID3D12Device>& device_cptr);
+    DeviceD3D12(const ComPtr<IDXGIAdapter>& adapter_cptr, const ComPtr<ID3D12Device>& device_cptr);
 private:
-    const ComPtr<ID3D12Device> m_device_cptr;
+    mutable ComPtr<ID3D12Device> m_device_cptr;
+    const ComPtr<IDXGIAdapter> m_adapter_cptr;
 };
 ARISENRHI_END_NAMESPACE
