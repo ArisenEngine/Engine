23429347e8318eddd602e9b3d4591940bb000f3a
Whisper <861314114@qq.com>
2025-08-20 15:04:56 +0800

    Update ContextCommonD3D12.h

--- Arisen/Core/ArisenRHID3D12/include/ContextCommonD3D12.h
+++ Arisen/Core/ArisenRHID3D12/include/ContextCommonD3D12.h
@@ -2,21 +2,24 @@
 #include "CommandQueueD3D12.h"
 #include "ContextBase.h"
 #include "CoreMinimalD3D12.h"
+#include "DescriptorManagerD3D12.h"
 #include "DeviceD3D12.h"
 
 ARISENRHI_D3D12_BEGIN_NAMEPSACE
 
-template<typename TContext, typename TSettings> //requires std::is_base_of_v<IRHIContext, TContext>
+template<typename TContext, typename TSettings> //requires std::is_base_of_v<ContextBase<>, TContext>
 class ContextCommonD3D12 : public TContext
 {
 public:
     ContextCommonD3D12(IDevice& device, const TSettings& settings)
-        :TContext(device, settings){}
+        :TContext(device, settings, MakeUniquePtr(DescriptorManagerD3D12, *this))
+    {}
 
     virtual void Initialize()
     {
         TContext::Initialize();
         // create descriptor.
+        GetDescriptorManagerD3D12().Iniitialize();
     }
 
     const DeviceD3D12& GetDeviceD3D12() const noexcept
@@ -33,7 +36,11 @@ public:
     {
         return static_cast<CommandQueueD3D12&>(ContextCommonD3D12::GetDefaultCommandKit(type).GetQueue());
     }
-    
+
+    DescriptorManagerD3D12& GetDescriptorManagerD3D12()
+    {
+        return static_cast<DescriptorManagerD3D12&>(ContextCommonD3D12::GetDescriptorManager());
+    }
 };
 
 
