a1a046a596e6eab40436cb8a7fb337d29765f246
Whisper <861314114@qq.com>
2025-08-20 15:02:54 +0800

    Update ContextBase.h

--- Arisen/Core/ArisenRHI/include/ContextBase.h
+++ Arisen/Core/ArisenRHI/include/ContextBase.h
@@ -12,6 +12,7 @@
 #include "IRHIContext.h"
 #include "IDevice.h"
 #include "DebugUtils/Checks.h"
+#include "DebugUtils/Verifies.h"
 
 ARISENRHI_BEGIN_NAMEPSACE
     static const std::array<std::string, static_cast<size_t>(CommandListType::__COUNT)> g_default_command_kit_names{
@@ -20,13 +21,17 @@ ARISENRHI_BEGIN_NAMEPSACE
     "Compute"
 };
 
-template<typename ContextInterface> requires std::is_base_of_v<IRHIContext , ContextInterface>
+template<typename RHIImplTraits, typename ContextInterface> requires std::is_base_of_v<IRHIContext , ContextInterface>
 class ContextBase : public ObjectBase<ContextInterface>
 {
 public:
-    ContextBase(IDevice& device, ContextType type)
+    using DescriptorManagerImplType = typename RHIImplTraits::DescriptorManagerImplType;
+    
+    ContextBase(IDevice& device, ContextType type, UniquePtr<DescriptorManagerImplType>&& descriptor_manager_ptr)
     :m_device_ptr(device.GetInterface<IDevice>())
-    ,m_type(type){}
+    ,m_type(type)
+    ,m_descriptor_manager_ptr(std::move(descriptor_manager_ptr))
+    {}
 
     virtual void Initialize()
     {
@@ -66,6 +71,13 @@ public:
         m_default_command_kit_ptrs_byQueue[std::addressof(cmd_kit_Ptr->GetQueue())] = cmd_kit_Ptr;
         return * cmd_kit_Ptr;
     }
+
+    virtual IDescriptorManager& GetDescriptorManager() override
+    {
+        VERIFY_NOT_NULL(m_descriptor_manager_ptr, "DescriptorManager is null!");
+        return *m_descriptor_manager_ptr;
+    }
+    
 private:
     typedef std::array<Ptr<ICommandKit>, static_cast<size_t>(CommandListType::__COUNT)> CommandKitPtrsByType;
     typedef std::map<ICommandQueue*, Ptr<ICommandKit>> CommandKitByQueue;
@@ -74,5 +86,7 @@ private:
     Ptr<IDevice> m_device_ptr;
     mutable CommandKitPtrsByType m_default_Command_Kit_Ptrs;
     mutable CommandKitByQueue m_default_command_kit_ptrs_byQueue;
+
+    UniquePtr<DescriptorManagerImplType> m_descriptor_manager_ptr;
 };
 ARISENRHI_END_NAMESPACE
