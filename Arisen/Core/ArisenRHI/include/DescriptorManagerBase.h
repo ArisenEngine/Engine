91b7ae1e861a4d57991a026c9b034b54f46a87c3
eatdreamcat <meow.std@outlook.com>
2025-08-20 09:51:38 +0800

    submit temp code

---
+++ Arisen/Core/ArisenRHI/include/DescriptorManagerBase.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "ContextBase.h"
+#include "CoreMinimalRHI.h"
+#include "IDescriptorManager.h"
+#include "ObjectBase.h"
+
+ARISENRHI_BEGIN_NAMEPSACE
+template<typename RHIImplTraits> requires std::is_base_of_v<IDescriptorManager,typename RHIImplTraits::DescriptorInterface>
+class DescriptorManagerBase : public ObjectBase<typename RHIImplTraits::DescriptorInterface>
+{
+public:
+    using BaseInterface = typename RHIImplTraits::DescriptorInterface;
+    
+    template<typename ContextInterface> 
+    DescriptorManagerBase(ContextBase<ContextInterface>& context)
+        :m_common_context(context)
+    {}
+private:
+    template<typename ContextInterface> 
+    ContextBase<ContextInterface>& m_common_context;
+};
+ARISENRHI_END_NAMESPACE
---
+++ Arisen/Core/ArisenRHID3D12/include/RHID3D12ImplTraits.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "CoreMinimalD3D12.h"
+
+ARISENRHI_D3D12_BEGIN_NAMEPSACE
+
+class DesciptorManagerD3D12;
+
+struct RHID3D12ImplTraits
+{
+    using DescriptorInterface = IDescriptorManager;
+    using DescriptorManagerImplType = DesciptorManagerD3D12;
+
+};
+ARISENRHI_D3D12_END_NAMESPACE
