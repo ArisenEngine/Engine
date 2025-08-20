60089206d2663cd2e6b8ccc8a976df9d9b58e868
Whisper <861314114@qq.com>
2025-08-20 15:05:15 +0800

    Update DescriptorManagerD3D12.h

--- Arisen/Core/ArisenRHID3D12/include/DescriptorManagerD3D12.h
+++ Arisen/Core/ArisenRHID3D12/include/DescriptorManagerD3D12.h
@@ -4,9 +4,19 @@
 #include "RHID3D12ImplTraits.h"
 
 ARISENRHI_D3D12_BEGIN_NAMEPSACE
+// Note: RTV/DSV Heaps are cpu visible heaps and SRV/SAMPLER Heaps are shader visible heaps.
+// We just need to copy the old descriptor heaps when add new descriptor heap for new resource,
+// but it's not suitable in the situation when we add a shader visible descriptor heap,instead
+// we should rebuild all the descriptor heaps and descriptor table bindings using IDescriptorManager::CompleteInitialize()
 class DescriptorManagerD3D12 : public DescriptorManagerBase<RHID3D12ImplTraits>
 {
 public:
+    explicit DescriptorManagerD3D12(IRHIContext& context);
+
+    virtual ~DescriptorManagerD3D12();
+
+    void Iniitialize();
+
     
 };
 ARISENRHI_D3D12_END_NAMESPACE
