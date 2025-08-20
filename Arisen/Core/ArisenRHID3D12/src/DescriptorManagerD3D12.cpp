25ef0e73dfd287220cdd0737fb55f6a677631a54
Whisper <861314114@qq.com>
2025-08-20 15:06:02 +0800

    Update DescriptorManagerD3D12.cpp

--- Arisen/Core/ArisenRHID3D12/src/DescriptorManagerD3D12.cpp
+++ Arisen/Core/ArisenRHID3D12/src/DescriptorManagerD3D12.cpp
@@ -1,1 +1,22 @@
 ﻿#include "DescriptorManagerD3D12.h"
+ARISENRHI_D3D12_BEGIN_NAMEPSACE
+
+DescriptorManagerD3D12::DescriptorManagerD3D12(IRHIContext& context)
+    :DescriptorManagerBase(context)
+{
+    LOG_RHI_CONSTRUCTOR("DescriptorManagerD3D12")
+}
+
+DescriptorManagerD3D12::~DescriptorManagerD3D12()
+{
+    LOG_RHI_DESTRUCTOR("DescriptorManagerD3D12")
+}
+
+void DescriptorManagerD3D12::Iniitialize()
+{
+    
+}
+
+
+ARISENRHI_D3D12_END_NAMESPACE
+No newline at end of file
