f65a902a7a1f10bae5d4e5f8406e960e15670ee7
eatdreamcat <meow.std@outlook.com>
2025-10-10 18:46:42 +0800

    初始化logger

--- Arisen/Test/ArisenEngineTest/Program.cs
+++ Arisen/Test/ArisenEngineTest/Program.cs
@@ -1,5 +1,7 @@
 
 using ArisenEngineTest.Shaders;
+
+ArisenEngine.Debug.Logger.Initialize();
 ArisenEngine.Debug.Logger.Log("###### Start Shader Lab Test ######");
 ShaderProcessor.ParseShader("ShaderLabRes/Packages/com.unity.render-pipelines.universal/Shaders", "SimpleLit.shader");
 ShaderProcessor.ParseShader("ShaderLabRes/Packages/com.unity.render-pipelines.universal/Shaders", "Lit.shader");
