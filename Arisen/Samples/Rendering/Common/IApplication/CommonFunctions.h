c55ff285a881745052475ddc5a622e7fd7ca448e
eatdreamcat <meow.std@outlook.com>
2026-01-29 15:27:16 +0800

    build: Implement comprehensive CMake build system for Arisen engine, including the RHI test case.

--- Arisen/Samples/Rendering/Common/IApplication/CommonFunctions.h
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-#include "Common.h"
-#include <vector>
-#include <fstream>
-
-extern DLL std::vector<char> AppReadFile(const std::string& filename);
--- Arisen/Samples/Rendering/Shader/GLSL/CompileShader.bat
+++
@@ -1,3 +0,0 @@
-%VULKAN_SDK%/Bin/glslc.exe ./SimpleTriangle.vert -o ./SimpleTriangle.vert.spv
-%VULKAN_SDK%/Bin/glslc.exe ./SimpleTriangle.frag -o ./SimpleTriangle.frag.spv
-pause
