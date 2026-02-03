eb4b89247e2f9d0ebc6965e1ed88e186cecf17dd
eatdreamcat <meow.std@outlook.com>
2026-02-03 16:34:11 +0800

    feat: Add glTF sample models for testing purposes.

--- Arisen/Test/NativeEngineTest/Main.cpp
+++ Arisen/Test/NativeEngineTest/Main.cpp
@@ -31,8 +31,8 @@ int main(int argc, char** argv)
     }
 
     // Register Tests
-    TestRunner::RegisterTest<RHIBindlessTest>();
-    TestRunner::RegisterTest<RHISyncTest>();
+    // TestRunner::RegisterTest<RHIBindlessTest>();
+    // TestRunner::RegisterTest<RHISyncTest>();
     // TestRunner::RegisterTest<RHIBasicRenderingTest>();
     TestRunner::RegisterTest<RHIGPUParticleTest>();
     
