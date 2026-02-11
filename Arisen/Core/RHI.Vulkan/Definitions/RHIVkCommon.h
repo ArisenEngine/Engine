bc82e8a77fb9495465d58e25e0fd8e5a90de644a
eatdreamcat <meow.std@outlook.com>
2026-02-11 17:42:41 +0800

    Fixing RenderDoc Startup Crash

--- Arisen/Core/RHI.Vulkan/Definitions/RHIVkCommon.h
+++ Arisen/Core/RHI.Vulkan/Definitions/RHIVkCommon.h
@@ -52,18 +52,23 @@ namespace ArisenEngine::RHI
     };
     
 
-    static ArisenEngine::Containers::Vector<const char*> VkDeviceExtensionNames
+    // device extensions
+    static ArisenEngine::Containers::Vector<const char*> VkMandatoryDeviceExtensionNames
     {
         VK_KHR_SWAPCHAIN_EXTENSION_NAME,
         VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME,
         VK_KHR_SYNCHRONIZATION_2_EXTENSION_NAME,
         VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME,
+        VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME
+    };
+
+    static ArisenEngine::Containers::Vector<const char*> VkOptionalDeviceExtensionNames
+    {
         VK_EXT_MESH_SHADER_EXTENSION_NAME,
         VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
         VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
         VK_KHR_RAY_QUERY_EXTENSION_NAME,
         VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME,
-        VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
         VK_EXT_ROBUSTNESS_2_EXTENSION_NAME
     };
 
