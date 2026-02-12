9a046223d0141f6e6678380b5132e5e890ca6ccf
eatdreamcat <meow.std@outlook.com>
2026-02-12 23:30:22 +0800

    feat: add new RHI ray tracing test case for rendering.

--- Arisen/Test/NativeEngineTest/RHI/Rendering/RHIRayTracingTest.h
+++ Arisen/Test/NativeEngineTest/RHI/Rendering/RHIRayTracingTest.h
@@ -306,7 +306,7 @@ namespace ArisenEngine::Testing
                 RHI::RHIAccelerationStructureGeometryData geom{};
                 geom.type = RHI::ERHIAccelerationStructureGeometryType::Triangles;
                 geom.flags = RHI::AS_GEOMETRY_OPAQUE_BIT;
-                geom.triangles.vertexFormat = RHI::FORMAT_R32G32B32A32_SFLOAT;
+                geom.triangles.vertexFormat = RHI::FORMAT_R32G32B32_SFLOAT;
                 geom.triangles.vertexData = RHI_Buffer_GetDeviceAddress(m_Device, m_Model.vertexBuffer);
                 geom.triangles.vertexStride = sizeof(GLTFVertex); // 64 bytes
                 geom.triangles.maxVertex = (UInt32)m_Model.vertexCount;
