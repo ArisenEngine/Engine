8547bdccafbee12d3d912f34809665be7d6eb7ac
eatdreamcat <meow.std@outlook.com>
2026-02-13 18:32:29 +0800

    feat: implement dynamic RHI loader for graphics APIs and a thread-local RHI error reporting system.

---
+++ Arisen/Core/Core.RHI/RHI/Loader/RHILoader.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "CoreRHICommon.h"
+#include "RHI/Definitions/GraphicsAPI.h"
+#include "RHI/Core/RHIInstance.h"
+
+namespace ArisenEngine::RHI
+{
+    /**
+     * @brief Responsible for loading and unloading RHI implementations (DLLs).
+     */
+    class CORE_RHI_DLL RHILoader
+    {
+    public:
+        NO_COPY_NO_MOVE_NO_DEFAULT(RHILoader)
+
+        /**
+         * @brief Set the current graphics API and load the corresponding implementation.
+         */
+        static void SetCurrentGraphicsAPI(GraphicsAPI api_type);
+
+        /**
+         * @brief Create an RHI instance using the currently loaded implementation.
+         */
+        static RHIInstance* CreateInstance(RHIInstanceInfo&& app_info);
+
+        /**
+         * @brief Unload the RHI implementation and cleanup.
+         */
+        static void Dispose();
+
+    private:
+        static inline GraphicsAPI _api_type { GraphicsAPI::None };
+        static inline void* _rhi_dll { nullptr }; // Using void* to avoid including Windows.h in public header
+    };
+} // namespace ArisenEngine::RHI
